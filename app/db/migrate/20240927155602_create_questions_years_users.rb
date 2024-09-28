fbbe016aef0ac0f110af983b738e482552f86f61
maximarquezregis <maximarquez2004@gmail.com>
2024-09-28 20:34:41 -0300

    fix migration table error

--- app/db/migrate/20240927155602_create_questions_years_users.rb
+++ app/db/migrate/20240927155602_create_questions_years_users.rb
@@ -1,7 +1,8 @@
 class CreateQuestionsYearsUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :questions_year_users do |t|
+    create_table :question_year_users do |t|
       t.references :user, null: false, foreign_key: true
+      t.references :question_year, null: false, foreign_key: true
       t.boolean :correct, default: false, null: false
     end
   end
