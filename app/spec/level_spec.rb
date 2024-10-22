5e75ed1d218048f00ce3ce63d253387aa1a8ecf8
Baachi13 <jeremiasavaro7@gmail.com>
2024-10-22 12:42:03 -0300

    Revert commit on wrong branch
    This reverts commit b8d84c9246bf006cfbae4ce5005188c41f554fa6.

--- app/spec/level_spec.rb
+++ app/spec/level_spec.rb
@@ -1,34 +1,33 @@
-# frozen_string_literal: true
-
 ENV['APP_ENV'] = 'test'
 
-require_relative '../models/level'
-require_relative '../models/planet'
-require_relative '../models/question'
+require_relative '../models/level.rb'
+require_relative '../models/planet.rb'
+require_relative '../models/question.rb'
 require 'rspec'
 require 'spec_helper'
 
 RSpec.describe Level, type: :model do
-  let(:planet) { Planet.create(name: 'Earth') }
+  let(:planet) { Planet.create(name: "Earth") }
 
-  it 'is valid with a number' do
+  it "is valid with a number" do
     level = Level.new(number: 1, planet: planet)
     expect(level).to be_valid
   end
 
-  it 'is invalid without a number' do
+  it "is invalid without a number" do
     level = Level.new(planet: planet)
     expect(level).not_to be_valid
   end
 
-  it 'is invalid if it has more than three questions' do
+  it "is invalid if it has more than three questions" do
     level = Level.create(number: 1, planet: planet)
-    (1..3).each do |_|
-      Question.create(description: 'Sample Question', level: level)
+    for i in 1..3 do
+      Question.create(description: "Sample Question", level: level)
     end
-    Question.create(description: 'Fourth Question', level: level)
+    Question.create(description: "Fourth Question", level: level)
     level.save
     expect(level).not_to be_valid
-    expect(level.errors[:questions]).to include('A level can have a maximum of 3 questions.')
+    expect(level.errors[:questions]).to include("A level can have a maximum of 3 questions.")
   end
+
 end
--- app/spec/planet_spec.rb
+++ app/spec/planet_spec.rb
@@ -1,32 +1,30 @@
-# frozen_string_literal: true
-
 ENV['APP_ENV'] = 'test'
 
-require_relative '../models/planet'
-require_relative '../models/level'
+require_relative '../models/planet.rb'
+require_relative '../models/level.rb'
 require 'rspec'
 require 'spec_helper'
 
 RSpec.describe Planet, type: :model do
-  it 'is valid with a name' do
-    planet = Planet.new(name: 'Earth')
+  it "is valid with a name" do
+    planet = Planet.new(name: "Earth")
     expect(planet).to be_valid
   end
 
-  it 'is invalid without a name' do
+  it "is invalid without a name" do
     planet = Planet.new
     expect(planet).not_to be_valid
     expect(planet.errors[:name]).to include("can't be blank")
   end
 
-  it 'is invalid if it has more than three levels' do
-    planet = Planet.create(name: 'Example Planet')
-    (1..3).each do |i|
+  it "is invalid if it has more than three levels" do
+    planet = Planet.create(name: "Example Planet")
+    for i in 1..3 do
       Level.create(number: i, planet: planet)
     end
     Level.create(number: 4, planet: planet)
     planet.save
     expect(planet).not_to be_valid
-    expect(planet.errors[:levels]).to include('A planet can have a maximum of 3 levels.')
+    expect(planet.errors[:levels]).to include("A planet can have a maximum of 3 levels.")
   end
 end
