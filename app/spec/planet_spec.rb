ebaefb66967da47723e5d3e5aab8bd202781802e
Baachi13 <jeremiasavaro7@gmail.com>
2024-10-29 11:39:01 -0300

    refactoring with new rubocop gems in spec done [#188458850]

--- app/spec/planet_spec.rb
+++ app/spec/planet_spec.rb
@@ -8,25 +8,45 @@ require 'rspec'
 require 'spec_helper'
 
 RSpec.describe Planet, type: :model do
-  it 'is valid with a name' do
-    planet = Planet.new(name: 'Earth')
-    expect(planet).to be_valid
-  end
-
-  it 'is invalid without a name' do
-    planet = Planet.new
-    expect(planet).not_to be_valid
-    expect(planet.errors[:name]).to include("can't be blank")
-  end
-
-  it 'is invalid if it has more than three levels' do
-    planet = Planet.create(name: 'Example Planet')
-    (1..3).each do |i|
-      Level.create(number: i, planet: planet)
+  describe 'validations' do
+    it 'is valid with a name' do
+      planet = described_class.new(name: 'Earth')
+      expect(planet).to be_valid
     end
-    Level.create(number: 4, planet: planet)
-    planet.save
-    expect(planet).not_to be_valid
-    expect(planet.errors[:levels]).to include('A planet can have a maximum of 3 levels.')
+
+    context 'when name is missing' do
+      let(:planet) { described_class.new }
+
+      it 'is invalid' do
+        expect(planet).not_to be_valid
+      end
+
+      it 'adds an error on the name attribute' do
+        planet.validate
+        expect(planet.errors[:name]).to include("can't be blank")
+      end
+    end
+
+    context 'with levels constraint' do
+      let(:planet) { described_class.create(name: 'Example Planet') }
+
+      it 'allows a planet to have up to 3 levels' do
+        (1..3).each { |i| Level.create(number: i, planet: planet) }
+        expect(planet).to be_valid
+      end
+
+      it 'is invalid if it has more than 3 levels' do
+        (1..3).each { |i| Level.create(number: i, planet: planet) }
+        Level.create(number: 4, planet: planet)
+        planet.validate
+        expect(planet).not_to be_valid
+      end
+
+      it 'adds an error on the levels attribute when exceeding the limit' do
+        (1..4).each { |i| Level.create(number: i, planet: planet) }
+        planet.validate
+        expect(planet.errors[:levels]).to include('A planet can have a maximum of 3 levels.')
+      end
+    end
   end
 end
--- app/spec/question_spec.rb
+++ app/spec/question_spec.rb
@@ -9,21 +9,32 @@ require 'rack/test'
 require 'spec_helper'
 
 RSpec.describe Question, type: :model do
-  it 'should have 4 answers' do
-    question = Question.create(description: 'Example question', level_id: 1)
-    answers = [
+  let(:question) { described_class.create(description: 'Example question', level_id: 1) }
+  let(:answers) do
+    [
       { description: 'a1', correct: false, question: question },
       { description: 'a2', correct: false, question: question },
       { description: 'a3', correct: true, question: question },
       { description: 'a4', correct: false, question: question }
     ]
-    Answer.create(answers)
-    expect(question.reload).to have_4_options
-    expect(question.correct_answer.description).to eq('a3')
   end
 
-  it 'is invalid without a description' do
-    question = Question.new(description: nil)
-    expect(question).not_to be_valid
+  describe 'answers' do
+    before { Answer.create(answers) }
+
+    it 'has 4 options' do
+      expect(question.reload).to have_4_options
+    end
+
+    it 'has the correct answer set as a3' do
+      expect(question.correct_answer.description).to eq('a3')
+    end
+  end
+
+  describe 'validations' do
+    it 'is invalid without a description' do
+      invalid_question = described_class.new(description: nil)
+      expect(invalid_question).not_to be_valid
+    end
   end
 end
